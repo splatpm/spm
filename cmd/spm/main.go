5aca43872e863e988f0795ad432d586f8339ffc0
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-08-13 11:37:29 -0700

    path updates

--- cmd/spm/main.go
+++ cmd/spm/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/urfave/cli.v2"
+	"gopkg.in/urfave/cli.v2"
 )
 
 func Stub(c *cli.Context) error {
@@ -23,14 +23,16 @@ func main() {
 		Version: "0.0.1",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
-				Name:  "debug",
-				Value: false,
-				Usage: "Turn on debugging output",
+				Name:    "debug",
+				Value:   false,
+				Usage:   "Turn on debugging output",
+				Aliases: []string{"D"},
 			},
 			&cli.BoolFlag{
-				Name:  "nocolor",
-				Value: false,
-				Usage: "Turn off colors in program output",
+				Name:    "nocolor",
+				Value:   false,
+				Usage:   "Turn off colors in program output",
+				Aliases: []string{"C"},
 			},
 		},
 		Commands: []*cli.Command{
@@ -39,19 +41,21 @@ func main() {
 			*/
 			{
 				Name:    "audit",
-				Aliases: []string{"A"},
+				Aliases: []string{"A", "au"},
 				Usage:   "Audit installed packages vs filesystem",
 				Action:  Stub,
 				Flags: []cli.Flag{
 					&cli.BoolFlag{
-						Name:  "report",
-						Usage: "Report on differences only.",
-						Value: true,
+						Name:    "report",
+						Usage:   "Report on differences only.",
+						Value:   true,
+						Aliases: []string{"p"},
 					},
 					&cli.BoolFlag{
-						Name:  "revert",
-						Usage: "Revert any filesystem changes to match recorded state",
-						Value: false,
+						Name:    "revert",
+						Usage:   "Revert any filesystem changes to match recorded state",
+						Value:   false,
+						Aliases: []string{"v"},
 					},
 				},
 			},
@@ -60,14 +64,15 @@ func main() {
 			*/
 			{
 				Name:    "install",
-				Aliases: []string{"I"},
+				Aliases: []string{"I", "in"},
 				Usage:   "Install a given package",
 				Action:  Stub,
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:  "root",
-						Usage: "Specify an alternate installation root",
-						Value: "/",
+						Name:    "root",
+						Usage:   "Specify an alternate installation root",
+						Value:   "/",
+						Aliases: []string{"r"},
 					},
 				},
 			},
@@ -76,9 +81,17 @@ func main() {
 			*/
 			{
 				Name:    "remove",
-				Aliases: []string{"R"},
+				Aliases: []string{"R", "rm"},
 				Usage:   "Remove a given package",
 				Action:  Stub,
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "force",
+						Usage:   "Force removal of package even if it breaks things",
+						Value:   false,
+						Aliases: []string{"f"},
+					},
+				},
 			},
 		},
 		Action: func(c *cli.Context) error {
